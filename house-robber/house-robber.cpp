e722895cf86797475f467598172b8cc0c1444216
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2021-10-12 12:10:15 +0530

    Time: 3 ms (44.40%), Space: 7.6 MB (72.44%) - LeetHub

---
+++ house-robber/house-robber.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0)
+            return 0;
+        else if(n==1)
+         {
+             return nums[0];
+         }
+       else if(n==2)
+        {
+            return max(nums[0],nums[1]);
+        }
+        nums[0]=nums[0];
+        nums[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++)
+        {
+            nums[i]=max(nums[i-1],nums[i]+nums[i-2]);
+        }
+        return nums[n-1];
+    }
+};
