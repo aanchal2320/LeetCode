c41c9f84f31b6354b7bace435c43503713f97bf3
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2021-11-20 03:10:00 +0530

    Time: 8 ms (95.30%), Space: 10.6 MB (71.72%) - LeetHub

---
+++ longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    //using dp tabulation method(bottom-up)
+    int longestCommonSubsequence(string text1,string text2)
+    {
+        int m=text1.length();
+        int n=text2.length();
+        int dp[m+1][n+1];
+        for(int k=0;k<=m;k++)
+        {
+            dp[k][0]=0;
+        }
+        for(int l=0;l<=n;l++)
+        {
+            dp[0][l]=0;
+        }
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
+    }
+};
