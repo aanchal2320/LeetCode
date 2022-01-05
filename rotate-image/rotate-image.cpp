b6669b4db69d84da4513d632c921d1a869344adc
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-01-05 14:56:06 +0530

    Time: 6 ms, Memory: 6.9 MB -LeetHub

--- rotate-image/rotate-image.cpp
+++ rotate-image/rotate-image.cpp
@@ -3,26 +3,23 @@ public:
     void rotate(vector<vector<int>>& a) {
         int m=a.size();
         int n=a[0].size();
-        //transpose of matrix
         for(int i=0;i<m;i++)
         {
             for(int j=i;j<n;j++)
             {
-                int temp=a[i][j];
+               int temp=a[i][j];
                 a[i][j]=a[j][i];
-                a[j][i]=temp;
+                a[j][i]=temp; 
             }
         }
-        //reversing matrix
-        int low=0;
-        int high=n-1;
+        int low=0,high=n-1;
         while(low<=high)
         {
             for(int i=0;i<n;i++)
             {
-                int temp=a[i][low];
-                a[i][low]=a[i][high];
-                a[i][high]=temp;
+              int temp=a[i][low];
+              a[i][low]=a[i][high];
+              a[i][high]=temp;
             }
             low++;
             high--;
