2e4e3fe7f331cf9b566dfcab3fbbfd88e0be0ad3
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-01-05 13:47:09 +0530

    Time: 28 ms, Memory: 25 MB -LeetHub

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void reverse(vector<int>&nums,int low,int high)
     {
-        while(low<high)
+        while(low<=high)
         {
             swap(nums[low],nums[high]);
             low++;
@@ -10,10 +10,10 @@ public:
         }
     }
     void rotate(vector<int>& nums, int k) {
-        int n=nums.size();
-        k=k%n;
-        reverse(nums,0,n-k-1);
-        reverse(nums,n-k,n-1);
-        reverse(nums,0,n-1);
+       int n=nums.size();
+       k=k%n;  //if k is greater than size
+       reverse(nums,0,n-1);
+       reverse(nums,0,k-1);
+       reverse(nums,k,n-1);
     }
 };
