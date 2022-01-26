3f04fd38a9ca87bad4d283d4653ac1c3f4d55b2e
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-01-27 02:04:05 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (47.52%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        int fact=1;
+        vector<int>v;
+        for(int i=1;i<n;i++)
+        {
+            fact=fact*i;
+            v.push_back(i);
+        }
+        v.push_back(n);
+        string ans="";
+        k=k-1;
+        while(true)
+        {
+            ans=ans+to_string(v[k/fact]);
+            v.erase(v.begin()+k/fact);
+            if(v.size()==0)
+            {
+                break;
+            }
+            k=k%fact;
+            fact=fact/v.size();
+        }
+        return ans;
+    }
+};
