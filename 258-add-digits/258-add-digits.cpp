4402f0ab449d29370b96761d12c7224f6c0875a4
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-02-08 13:31:43 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.57%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num>9)
+        {
+            int sum=0;
+            while(num)
+            {
+                sum+=num%10;
+                num=num/10;
+            }
+            num=sum;
+        }
+        return num;
+    }
+};
+
