718b601317b35046dcd23a6f53d7a8f8a32c1715
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-02-15 00:45:17 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.34%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        unsigned int carry;
+        while(b!=0)
+        {
+            carry=a&b;
+            a=a^b;
+            b=carry<<1;
+        }
+        return a;
+    }
+};
