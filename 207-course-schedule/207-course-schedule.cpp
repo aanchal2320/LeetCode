6e48b01c5e7e5d045e9f62a632a719f66056a552
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-02-21 00:49:16 +0530

    Time: 23 ms (72.68%), Space: 14.5 MB (33.46%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+   bool dfs(int i, vector<int>&cur , vector<int>&visited)
+    {
+        visited[i]=1;
+        cur[i]=1;
+        for(auto x:edge[i])
+        {
+           if(!visited[x])
+           {
+               if (  dfs(x,cur,visited)==false )return false;
+           }
+           else if(cur[x])return false; 
+            
+            
+        }
+        cur[i]=0;
+        return true;
+    }
+public:
+      vector<vector<int>>edge;
+      bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        
+          
+        edge.resize(n+1);
+        for(auto x:prerequisites)
+        {
+            edge[x[0]].push_back(x[1]);
+        }
+    
+         vector<int>visited(n+1,0);
+         vector<int>cur(n+1,0);
+          for(int i=0;i<n;i++){
+           
+            if(visited[i]==0)
+            {
+                 if(!dfs(i,cur,visited)) return false ;
+            }
+           
+               
+        }
+        return true;
+    }
+    
+};
