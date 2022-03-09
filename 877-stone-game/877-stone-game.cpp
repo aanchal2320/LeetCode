87d0a0d60a88d816ac78839ddf22e2be603456d7
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-03-10 00:41:35 +0530

    Time: 27 ms (30.36%), Space: 8.5 MB (44.10%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
+    int memo[501][501];
+    int func(int i,int j,vector<int>&piles)
+    {
+        if(i>j)
+            return 0;
+        if(memo[i][j]!=0)
+            return memo[i][j];
+        return memo[i][j]=max(piles[i]-func(i+1,j,piles),piles[j]-func(i,j-1,piles));
+    }
     bool stoneGame(vector<int>& piles) {
-        return 1;
+       int n=piles.size();
+    return func(0,n-1,piles)>0;
     }
 };
