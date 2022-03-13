e7a83af001cb23372813bf14ce9f01ae5fd7f8a6
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-03-13 09:36:59 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (96.29%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char>s;
+        for(auto i:str)
+        {
+            if(i=='('||i=='['||i=='{')
+            {
+                s.push(i);
+            }
+            else
+            {
+                if(s.empty()||(s.top()=='('&&i!=')')||(s.top()=='{'&&i!='}')||(s.top()=='['&&i!=']'))
+                    return false;
+                s.pop();
+            }
+        }
+        if(s.empty())
+            return true;
+        else
+            return false;
+    }
+};
