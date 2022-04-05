bdab3a97b80926a65e625a0642801cec0f380db2
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-04-05 23:51:56 +0530

    Time: 317 ms (23.38%), Space: 83.1 MB (26.27%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,46 +1,15 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& nums) {
-        bool one=false;
-        int n=nums.size();
-        for(auto x: nums)
-        {
-            if(x==1)
-            {
-                one=true;
-                break;
-            }
-        }
-        if(!one)
-        {
-            return 1;
-        }
-        if(n==1)
-        {
-            return 2;
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>n || nums[i]<=0)
-            {
-                nums[i]=1;
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            int x=abs(nums[i]);
-            if(nums[x-1]>0)
-            {
-                nums[x-1]=(-1)*nums[x-1];
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>0)
-            {
-                return i+1;
-            }
-        }
-        return n+1;
+    int firstMissingPositive(vector<int>& A) {
+        int n=A.size();
+       for(int i = 0; i < n; ++ i)
+            while(A[i] > 0 && A[i] <= n && A[A[i] - 1] != A[i])
+                swap(A[i], A[A[i] - 1]);
+        
+        for(int i = 0; i < n; ++ i)
+            if(A[i] != i + 1)
+                return i + 1;
+        
+        return n + 1;
     }
 };
