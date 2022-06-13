e7de598af0557f60891ac74c58e7b1470b88fcd2
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-06-13 19:24:01 +0530

    Time: 13 ms (22.10%), Space: 8.7 MB (55.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,21 +1,7 @@
 class Solution {
-    // int func(int i,int j,int n,vector<vector<int>>&triangle, vector<vector<int>>&dp)
-    // {
-    //     if(i==n-1)
-    //     {
-    //       return triangle[n-1][j];
-    //     }
-    //     if(dp[i][j]!=-1)
-    //     {
-    //         return dp[i][j];
-    //     }
-    //     int down=triangle[i][j]+func(i+1,j,n,triangle,dp);
-    //     int diagonal=triangle[i][j]+func(i+1,j+1,n,triangle,dp);
-    //     return dp[i][j]=min(down,diagonal);
-    // }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
+       int n=triangle.size();
         //memoization
         // vector<vector<int>>dp(n,vector<int>(n,-1));
         // int ans=func(0,0,n,triangle,dp);
