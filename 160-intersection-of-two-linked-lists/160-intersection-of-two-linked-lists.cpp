f365ddc52d93c7ed2f7c42368e0de6cc349d9b33
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-06-23 21:37:28 +0530

    Time: 54 ms (76.41%), Space: 14.6 MB (72.26%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -13,22 +13,8 @@ public:
        ListNode* d2=h2;
        while(d1!=d2)
        {
-           if(d1==NULL)
-           {
-               d1=h2;
-           }
-           else
-           {
-               d1=d1->next;
-           }
-           if(d2==NULL)
-           {
-               d2=h1;
-           }
-           else
-           {
-               d2=d2->next;
-           }
+           d1=d1==NULL?h2:d1->next;
+           d2=d2==NULL?h1:d2->next;
        }
         return d2;
     }
