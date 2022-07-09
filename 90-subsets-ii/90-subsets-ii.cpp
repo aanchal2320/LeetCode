2bcd5d292706677e7350ee4cc3c315617de7078d
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-07-10 02:06:43 +0530

    Time: 4 ms (69.26%), Space: 7.6 MB (60.96%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void findsubset(int ind,vector<int>&nums,vector<int>&ds,vector<vector<int>>&ans)
+    void func(int ind,vector<vector<int>>&ans,vector<int>&ds,vector<int>nums)
     {
         ans.push_back(ds);
         for(int i=ind;i<nums.size();i++)
@@ -10,15 +10,15 @@ public:
                 continue;
             }
             ds.push_back(nums[i]);
-            findsubset(i+1,nums,ds,ans);
+            func(i+1,ans,ds,nums);
             ds.pop_back();
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-       vector<vector<int>>ans;
-        vector<int>ds;
         sort(nums.begin(),nums.end());
-        findsubset(0,nums,ds,ans);
+        vector<vector<int>>ans;
+        vector<int>ds;
+        func(0,ans,ds,nums);
         return ans;
     }
 };
