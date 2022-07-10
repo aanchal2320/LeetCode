8043d6f19158ca8cc0ec61c5b3c962fb1afd7334
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-07-10 19:44:33 +0530

    Time: 153 ms (19.30%), Space: 69.2 MB (14.02%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-void combinations(int i,int target,vector<int>&candidates,vector<vector<int>>&res,vector<int>&a)
+    void func(int ind,int target,vector<int>candidates,vector<vector<int>>&ans,vector<int>&ds)
     {
-        if(i==candidates.size())
+        if(ind==candidates.size())
         {
             if(target==0)
             {
-                res.push_back(a);
+                ans.push_back(ds);
+                return;
             }
-            return;
+            else
+            {
+                return;
+            }
         }
-    if(candidates[i]<=target)
-    {
-        a.push_back(candidates[i]);
-        combinations(i,target-candidates[i],candidates,res,a);
-        a.pop_back();
-    }
-    combinations(i+1,target,candidates,res,a);
+        if(candidates[ind]<=target)
+        {
+            ds.push_back(candidates[ind]);
+            func(ind,target-candidates[ind],candidates,ans,ds);
+            ds.pop_back();
+        }
+        func(ind+1,target,candidates,ans,ds);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>res;
-        vector<int>a;
-        combinations(0,target,candidates,res,a);
-        return res;
+        vector<vector<int>>ans;
+        vector<int>ds;
+        func(0,target,candidates,ans,ds);
+        return ans;
     }
 };
