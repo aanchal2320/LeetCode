3ac4940fbe7284c18d4e7e5451f44f40c9818eed
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-07-12 00:33:15 +0530

    Time: 8 ms (70.95%), Space: 7 MB (95.64%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    bool issafe(int row,int col,vector<string>board,int n)
+    bool ispossible(int row,int col,vector<string>&board,int n)
     {
-        int duprow=row;
-        int dupcol=col;
+        int r=row;
+        int c=col;
         while(row>=0 && col>=0)
         {
             if(board[row][col]=='Q')
                 return false;
-            row--;
-            col--;
+             row--;
+             col--;
         }
-        col=dupcol;
-        row=duprow;
+        row=r;
+        col=c;
         while(col>=0)
         {
             if(board[row][col]=='Q')
                 return false;
             col--;
         }
-        row=duprow;
-        col=dupcol;
+        row=r;
+        col=c;
         while(row<n && col>=0)
         {
             if(board[row][col]=='Q')
@@ -30,20 +30,20 @@ public:
         }
         return true;
     }
-    void solve(int col,vector<string>&board,vector<vector<string>>&ans,int n)
+    void func(int col,vector<vector<string>>&ans,vector<string>&board,int n)
     {
         if(col==n)
         {
             ans.push_back(board);
             return;
         }
-        for(int row=0;row<n;row++)
+        for(int i=0;i<n;i++)
         {
-            if(issafe(row,col,board,n))
+            if(ispossible(i,col,board,n))
             {
-                board[row][col]='Q';
-                solve(col+1,board,ans,n);
-                board[row][col]='.';
+                board[i][col]='Q';
+                func(col+1,ans,board,n);
+                board[i][col]='.';
             }
         }
     }
@@ -55,7 +55,7 @@ public:
         {
             board[i]=s;
         }
-        solve(0,board,ans,n);
+        func(0,ans,board,n);
         return ans;
     }
 };
