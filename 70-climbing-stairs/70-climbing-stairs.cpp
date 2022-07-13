c1b91638e338b5d739116122db82eb4aa4c82464
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-07-13 19:08:44 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.64%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        dp[0]=1,dp[1]=1;
+        //Space optimization
+        int prev1=1;
+        int prev2=1;
         for(int i=2;i<=n;i++)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
         }
-        return dp[n];
+        return prev1;
+        //DP solution
+        // vector<int>dp(n+1,-1);
+        // dp[0]=1,dp[1]=1;
+        // for(int i=2;i<=n;i++)
+        // {
+        //     dp[i]=dp[i-1]+dp[i-2];
+        // }
+        // return dp[n];
     }
 };
