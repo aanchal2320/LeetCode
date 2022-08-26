1746e359f6d78fcdcc326d1bfdae35180f1140e5
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-08-27 00:40:26 +0530

    Time: 963 ms (33.32%), Space: 14.1 MB (68.54%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string>s;
-        bool ispresent=false;
-        for(auto word: wordList)
+    int ladderLength(string beginword, string endword, vector<string>& wordlist) {
+        if(find(wordlist.begin(),wordlist.end(),endword)==wordlist.end())
         {
-            if(endWord.compare(word)==0)
-                ispresent=true;
-            s.insert(word);
-        }
-        if(ispresent==false)
             return 0;
+        }
+        set<string>s;
+        for(auto x:wordlist)
+        {
+            s.insert(x);
+        }
         queue<string>q;
-        q.push(beginWord);
+        q.push(beginword);
         int depth=0;
-        while(q.empty()==false)
+        while(!q.empty())
         {
             depth++;
-            int lsize=q.size();
-            while(lsize--)
+            int n=q.size();
+            while(n--)
             {
-                string curr=q.front();
-                q.pop();
-                for(int i=0;i<curr.length();i++)
+            string curr=q.front();
+            q.pop();
+            for(int i=0;i<curr.size();i++)
+            {
+                string temp=curr;
+                for(char c='a';c<='z';c++)
                 {
-                    string temp=curr;
-                    for(char c='a';c<='z';c++)
-                    {
-                        temp[i]=c;
-                        if(curr.compare(temp)==0)
-                            continue;
-                        if(temp.compare(endWord)==0)
-                            return depth+1;
-                        if(s.find(temp)!=s.end())
-                        {
-                            q.push(temp);
-                            s.erase(temp);
-                        }
-                    }
+                    temp[i]=c;
+                    if(temp==curr)
+                        continue;
+                    if(temp==endword)
+                        return depth+1;
+                    if(s.find(temp)!=s.end())
+                        q.push(temp);
+                        s.erase(temp);
                 }
             }
         }
+    }
         return 0;
     }
 };
