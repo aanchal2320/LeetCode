3644e8844b17cd35ab8478f98f43d7d7eacdab6f
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-09-22 22:35:44 +0530

    Time: 10 ms (98.17%), Space: 13.2 MB (82.08%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& a) {
+    void setZeroes(vector<vector<int>>&a) {
         int m=a.size();
         int n=a[0].size();
-        int first=1;
+        int flag=0;
         for(int i=0;i<m;i++)
         {
             if(a[i][0]==0)
             {
-                first=0;
+                flag=1;
             }
             for(int j=1;j<n;j++)
             {
                 if(a[i][j]==0)
                 {
-                    a[0][j]=0;
                     a[i][0]=0;
+                    a[0][j]=0;
                 }
             }
         }
         for(int i=m-1;i>=0;i--)
-         {
-                for(int j=n-1;j>=1;j--)
-                {
-                    if(a[0][j]==0 || a[i][0]==0)
-                        {
-                            a[i][j]=0;
-                        }
-                }
-            if(first==0)
+        {
+            for(int j=n-1;j>=1;j--)
             {
-                 a[i][0]=0;
+                if(a[i][0]==0 || a[0][j]==0)
+                {
+                    a[i][j]=0;
+                }
+            }
+            if(flag==1)
+            {
+                a[i][0]=0;
             }
         }
     }
