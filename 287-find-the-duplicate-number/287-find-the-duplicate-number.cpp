6b049ddb285285552fac8a211bb459b652b9298f
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2022-09-29 13:37:46 +0530

    Time: 320 ms (22.93%), Space: 61.3 MB (62.14%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -8,7 +8,7 @@ public:
             slow=nums[slow];
             fast=nums[nums[fast]];
         }while(slow!=fast);
-        fast=nums[0];
+        slow=nums[0];
         while(slow!=fast)
         {
             slow=nums[slow];
