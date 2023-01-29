6cdcf93ca3dcc12e291b4f324bcb84fc0d381743
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2023-01-29 21:04:31 +0530

    Added solution - LeetHub

--- Minimum number of jumps - GFG/minimum-number-of-jumps.cpp
+++ Minimum number of jumps - GFG/minimum-number-of-jumps.cpp
@@ -1,50 +1,51 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 // Function to return minimum number of jumps to end of array
 
 class Solution{
   public:
     int minJumps(int arr[], int n){
-        int maxrange=arr[0],steps=arr[0],jump=1;
-        if(n<=1)
-        {
-            return 0;
-        }
-        else if(arr[0]==0)
-        {
-            return -1;
-        }
-        else 
-        {
-        for(int i=1;i<n;i++)
-        {
-            if(i==n-1)
-            {
-                return jump;
-            }
-            maxrange=max(maxrange,arr[i]+i);
-            steps--;
-            if(steps==0)
-            {
-                jump++;
-                if(i>=maxrange)
-                {
-                    return -1;
-                }
-                steps=maxrange-i;
-            }
-        }
-        }
-        return jump;
+       int maxR=arr[0];
+       int steps=arr[0];
+       int jumps=1;
+       if(n==1)
+       {
+           return 0;
+       }
+       else if(arr[0]==0)
+       {
+           return -1;
+       }
+       else
+       {
+           for(int i=1;i<n;i++)
+           {
+               if(i==n-1)
+               {
+                   return jumps;
+               }
+               maxR=max(maxR,i+arr[i]);
+               steps--;
+               if(steps==0)
+               {
+                   jumps++;
+                   if(i>=maxR)
+                   {
+                       return -1;
+                   }
+                   steps=maxR-i;
+               }
+           }
+       }
     }
 };
 
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main()
 {
@@ -62,4 +63,5 @@ int main()
     }
     return 0;
 }
-  // } Driver Code Ends
+
+// } Driver Code Ends
